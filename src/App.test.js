30a1d2ffe622ed1c7180a14336af3d52e4575947
Adam Jones <domdomegg+git@gmail.com>
2024-09-03 10:03:29 +0100

    Set up MRE

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { expect, test } from '@jest/globals';
+test('Jimp can load', () => {
+  // eslint-disable-next-line global-require
+  expect(() => require('jimp')).not.toThrow();
 });
